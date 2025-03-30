cd5d69e5f4043c2009dd6fb789a3dd39706596fd
Max McDaniel <maxhmcd08@gmail.com>
2025-03-30 10:23:24 -0600

    Update for current school year. Add confetti once countdown ends. Start working on notification system, doesn't appear on mobile for iOS?

--- index.js
+++ index.js
@@ -1,21 +1,72 @@
-let counter = document.getElementById('counter');
+const counter = document.getElementById('counter');
+const subheader = document.getElementById('subheader');
+const allowNotificationsButton = document.getElementById('allow-notifications');
+
+if (!"Notification" in window) allowNotificationsButton.style.display = "none";
+
+allowNotificationsButton.addEventListener('click', (e) => {
+    Notification.requestPermission().then((result) => {
+        if (result !== "granted") {
+            if (result == "denied") alert("You previously denied notifications, please reset the setting then try again.");
+            return;
+        };
+    });
+
+    if (Notification.permission == "granted") {
+        let n = new Notification("School Countdown", { body: "Testing 123" });
+    };
+});
 
 function count() {
     let now = Date.now();
-    let schoolEnd = 1727884800000;
+    let schoolEnd = 1749241500000;
     let difference = schoolEnd - now;
 
     if (now >= schoolEnd) {
-        counter.innerText = "0 DAYS!";
+        counter.style.display = "none";
+        subheader.innerText = "School has Ended!";
+        subheader.style.fontSize = "5rem";
+
+        // Left Confetti
+        confetti({
+            particleCount: 100,
+            spread: 100,
+            angle: 135,
+            origin: {
+                y: -.2,
+                x: 0.4
+            }
+        });
+
+        // Center Confetti
+        confetti({
+            particleCount: 100,
+            spread: 100,
+            angle: 90,
+            origin: {
+                y: 0
+            }
+        });
+
+        // Right Confetti
+        confetti({
+            particleCount: 100,
+            spread: 100,
+            angle: 45,
+            origin: {
+                y: -.2,
+                x: 0.6
+            }
+        });
         return;
     };
 
-    let days = Math.floor(difference / (1000 * 60 * 60 * 24));
-    let hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    let minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
-    let seconds = Math.floor((difference % (1000 * 60)) / 1000);
+    let days = `<span style="color: #fb2c36;">${Math.floor(difference / (1000 * 60 * 60 * 24))}d</span>`;
+    let hours = `<span style="color: #ff6900;">${Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))}h</span>`;
+    let minutes = `<span style="color: #f0b100;">${Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60))}m</span>`;
+    let seconds = `<span style="color: #22c55e;">${Math.floor((difference % (1000 * 60)) / 1000)}s</span>`;
 
-    counter.innerText = `${days}d : ${hours}h : ${minutes}m : ${seconds}s`;
+    counter.innerHTML = `${days} : ${hours} : ${minutes} : ${seconds}`;
 };
 
 count();
